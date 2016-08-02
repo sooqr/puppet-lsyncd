822f5c052c62a3a0d2018a25753d6ae399372993
Eric Bus <eric.bus@sooqr.com>
2016-08-02 13:46:20 +0200

    Template now uses config path from csync2 module csync2_sources is now a parameter of the class. This enables the use of the template() call inside the scope of the class.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,17 +6,39 @@
 #  class { 'lsyncd': config_source => 'puppet:///modules/example/lsyncd.conf' }
 #
 class lsyncd (
-  $config_source  = undef,
-  $config_content = undef,
-  $logdir_owner   = 'root',
-  $logdir_group   = 'root',
-  $logdir_mode    = '0755',
-  $lsyncd_options = undef,
+  $config_source   = undef,
+  $config_content  = undef,
+  $logdir_owner    = 'root',
+  $logdir_group    = 'root',
+  $logdir_mode     = '0755',
+  $lsyncd_options  = undef,
   # This parameter requires a modified rpm init script
-  $lsyncd_user    = undef,
+  $lsyncd_user     = undef,
+  $config_path     = $lsyncd::params::config_path,
+  $config_file     = $lsyncd::params::config_file,
+  $csync2_sources  = undef,
 ) inherits lsyncd::params {
 
   package { 'lsyncd': ensure => 'installed' }
+
+  # validate values supplied
+  validate_absolute_path($config_path)
+
+  # Create the configuration base path
+  if $::osfamily =~ /(Debian|Ubuntu)/ {
+    file { $config_path:
+      ensure => directory,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+    }
+  }
+
+  if ($csync2_sources) {
+    $_content = template('lsyncd/lsyncd-csync2.conf.erb')
+  } else {
+    $_content = $config_content
+  }
 
   service { 'lsyncd':
     ensure    => 'running',
@@ -25,28 +47,29 @@ class lsyncd (
     require   => Package['lsyncd'],
   }
 
-  file { $lsyncd::params::cfg_file:
+  file { $config_file:
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     source  => $config_source,
-    content => $config_content,
+    content => $_content,
     require => Package['lsyncd'],
     notify  => Service['lsyncd'],
   }
 
-  file { '/etc/sysconfig/lsyncd':
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template("${module_name}/lsyncd-sysconfig.erb"),
-    require => Package['lsyncd'],
-    notify  => Service['lsyncd'],
-  }
-
-  # As of 2.1.4-3.el6 the rpm package doesn't include these directories
-  # Later versions do, but we might need to change permissions
+  # Debian does not support/require additional configuration file
   if $::osfamily !~ /(Debian|Ubuntu)/ {
+    file { '/etc/sysconfig/lsyncd':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template("${module_name}/lsyncd-sysconfig.erb"),
+      require => Package['lsyncd'],
+      notify  => Service['lsyncd'],
+    }
+
+    # As of 2.1.4-3.el6 the rpm package doesn't include these directories
+    # Later versions do, but we might need to change permissions
     file { [ '/var/log/lsyncd', '/var/run/lsyncd' ]:
       ensure  => 'directory',
       owner   => $logdir_owner,
