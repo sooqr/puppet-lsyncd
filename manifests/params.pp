a60f98da4af19ec731a8fb3798e5ece3686eb5a9
Eric Bus <eric.bus@sooqr.com>
2016-08-02 13:45:39 +0200

    Changed from ternary to case syntax

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,11 +1,13 @@
 class lsyncd::params {
-  $cfg_path = $::osfamily ? {
-    /(Debian|Ubuntu)/ => '/etc/lsyncd',
-    default => '/etc',
-  }
-    
-  $cfg_file = $::osfamily ? {
-    /(Debian|Ubuntu)/ => "${cfg_path}/lsyncd.conf.lua",
-    default => "${cfg_path}/lsyncd.conf",
+  case $::osfamily {
+    'Debian': {
+      $config_path = '/etc/lsyncd'
+      $config_file = "${config_path}/lsyncd.conf.lua"
+    }
+
+    default: {
+      $config_path = '/etc'
+      $config_file = "${config_path}/lsyncd.conf"
+    }
   }
 }
